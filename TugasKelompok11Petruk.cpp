2a8ed44601691b45ea5f084005a3493c2d26312d
Rayhan Akbar <86936505+RayhanEVM@users.noreply.github.com>
2024-04-28 16:31:49 +0700

    Update TugasKelompok11Petruk.cpp

--- TugasKelompok11Petruk.cpp
+++ TugasKelompok11Petruk.cpp
@@ -14,19 +14,4 @@ bool searchHorizontal(vector<string>& matrix, string word) {
 }
 
 
-bool searchVertical(vector<string>& matrix, string word) {
-    int row = matrix.size();
-    int col = matrix[0].size();
-
-    for (int j = 0; j < col; ++j) {
-        string column;
-        for (int i = 0; i < row; ++i) {
-            column += matrix[i][j];
-        }
-        if (column.find(word) != string::npos) {
-            return true;
-        }
-    }
-    return false;
-}
 
