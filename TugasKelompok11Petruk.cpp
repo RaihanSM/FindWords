5b4dd7c43ee26da2fce8d451fb00545fc3c70857
RaihanSM <83532862+RaihanSM@users.noreply.github.com>
2024-04-28 19:18:58 +0700

    Update TugasKelompok11Petruk.cpp

--- TugasKelompok11Petruk.cpp
+++ TugasKelompok11Petruk.cpp
@@ -13,5 +13,44 @@ bool searchHorizontal(vector<string>& matrix, string word) {
     return false;
 }
 
+bool searchDiagonal(vector<string>& matrix, string word) {
+    int row = matrix.size();
+    int col = matrix[0].size();
+
+    for (int i = 0; i < row; ++i) {
+        for (int j = 0; j < col; ++j) {
+            string diagonal;
+            int r = i;
+            int c = j;
+            while (r < row && c < col) {
+                diagonal += matrix[r][c];
+                ++r;
+                ++c;
+            }
+            if (diagonal.find(word) != string::npos) {
+                return true;
+            }
+        }
+    }
+
+    for (int i = 0; i < row; ++i) {
+        for (int j = 0; j < col; ++j) {
+            string diagonal;
+            int r = i;
+            int c = j;
+            while (r >= 0 && c < col) {
+                diagonal += matrix[r][c];
+                --r;
+                ++c;
+            }
+            if (diagonal.find(word) != string::npos) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
 
 
